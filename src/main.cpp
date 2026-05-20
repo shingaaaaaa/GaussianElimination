dc35a37ed266afb46711cd45dd6ef94c6ce5c626
elizavetasingareva <lizikborts@gmail.com>
2026-05-21 02:53:38 +0300

    feat(main): call readMatrix(), gaussianElimination(), writeResult()

--- src/main.cpp
+++ src/main.cpp
@@ -28,9 +28,31 @@ int main(int argc, char* argv[])
     }
     else
     {
-        // TODO: реализовать чтение матрицы, решение и запись результата
-        std::cerr << "Логика решения ещё не реализована." << std::endl;
-        exitCode = 1;
+        const std::string inputPath = argv[1];
+        const std::string outputPath = argv[2];
+
+        Matrix matrix;
+        std::vector<Error> errors;
+
+        const bool readOk = readMatrix(inputPath, matrix, errors);
+        if (!readOk)
+        {
+            // Пока выводим только первое сообщение об ошибке (временное решение)
+            if (!errors.empty())
+                std::cerr << errors[0].generateErrorMessage() << std::endl;
+            exitCode = 1;
+        }
+        else
+        {
+            std::vector<double> solution;
+            const SolutionType solutionType = gaussianElimination(matrix, solution);
+            const bool writeOk = writeResult(outputPath, solutionType, solution);
+            if (!writeOk)
+            {
+                std::cerr << "Ошибка: невозможно создать выходной файл." << std::endl;
+                exitCode = 1;
+            }
+        }
     }
     return exitCode;
 }
